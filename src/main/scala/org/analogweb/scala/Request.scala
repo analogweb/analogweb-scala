705e42f8aed907170277d6d3e2821be9e5f8a0a6
snowgooseyk <snowgoose.yk@gmail.com>
2014-05-27 01:11:19 +0900

    Add method.

--- src/main/scala/org/analogweb/scala/Request.scala
+++ src/main/scala/org/analogweb/scala/Request.scala
@@ -1,9 +1,12 @@
 package org.analogweb.scala
 
 import org.analogweb.RequestContext
+import org.analogweb.RequestValueResolvers
+import org.analogweb.InvocationMetadata
+import org.analogweb.core.ParameterValueResolver
 import collection.JavaConversions._
 
-class Request(rc : RequestContext) {
+class Request(rc : RequestContext,rvr : RequestValueResolvers,im : InvocationMetadata) {
 
   def parameter(name: String):Option[String] = {
     parameters(name).headOption
@@ -13,4 +16,19 @@ class Request(rc : RequestContext) {
     rc.getQueryParameters.getValues(name)
   }
 
+  def header(name:String) = {
+    headers(name).headOption
+  }
+
+  def headers(name:String) = {
+    rc.getRequestHeaders.getValues(name)
+  }
+
+  def attribute(name:String) = {
+    attributes(name).headOption
+  }
+  
+  def attributes(name:String) = {
+    rvr.findRequestValueResolver(classOf[ParameterValueResolver]).resolveValue(rc,im,name,classOf[List[String]])
+  }
 }
