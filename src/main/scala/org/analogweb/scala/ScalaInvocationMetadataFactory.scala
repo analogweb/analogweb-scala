131ca947ef5a9925cd7010156638db9dc95933ee
snowgooseyk <snowgoose.yk@gmail.com>
2014-05-30 00:57:08 +0900

    Refactor

--- src/main/scala/org/analogweb/scala/ScalaInvocationMetadataFactory.scala
+++ src/main/scala/org/analogweb/scala/ScalaInvocationMetadataFactory.scala
@@ -4,27 +4,32 @@ import collection.JavaConversions._
 import org.analogweb.InvocationMetadata
 import org.analogweb.InvocationMetadataFactory
 import org.analogweb.core.DefaultInvocationMetadata
-import java.util
+import java.util.Collection
+import java.util.Collections
 import scala.collection.mutable.ArrayBuffer
 
 class ScalaInvocationMetadataFactory extends InvocationMetadataFactory {
+
+  val ignoreMethods = Seq("get", "post", "put", "delete")
+
   def containsInvocationClass(clazz: Class[_]): Boolean = {
     classOf[Analogweb].isAssignableFrom(clazz) && classOf[Analogweb].getCanonicalName != clazz.getCanonicalName
   }
 
-  def createInvocationMetadatas(clazz: Class[_]): util.Collection[InvocationMetadata] = {
-    val metadatas = ArrayBuffer[InvocationMetadata]()
+  def createInvocationMetadatas(clazz: Class[_]): Collection[InvocationMetadata] = {
     val classOfAnalogweb = classOf[Analogweb]
-    val instance = clazz.newInstance
     clazz match {
       case classOfAnalogweb => {
-        clazz.getDeclaredMethods.filter(m =>
+        val ms = clazz.getDeclaredMethods.filter(m =>
           classOf[Route].isAssignableFrom(m.getReturnType)).filter(m =>
-          Seq("get", "post", "put", "delete").contains(m.getName) == false).map(f =>
-          metadatas.append(new ScalaInvocationMetadata(clazz, f.getName, null, f.invoke(instance).asInstanceOf[Route])))
+          ignoreMethods.contains(m.getName) == false
+        )
+        val instance = clazz.newInstance
+        ms.map(f =>
+          new ScalaInvocationMetadata(clazz, f.getName, null, f.invoke(instance).asInstanceOf[Route])).toSeq
       }
+      case _ => new java.util.ArrayList[InvocationMetadata]
     }
-    metadatas
   }
 
 }
