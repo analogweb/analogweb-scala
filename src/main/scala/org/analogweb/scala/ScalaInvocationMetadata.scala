f4da2312b0293877b7e487e8be38f67a7ae9ec5e
snowgooseyk <snowgoose.yk@gmail.com>
2014-05-30 10:19:45 +0900

    Remove unused import.

--- src/main/scala/org/analogweb/scala/ScalaInvocationMetadata.scala
+++ src/main/scala/org/analogweb/scala/ScalaInvocationMetadata.scala
@@ -1,9 +1,6 @@
 package org.analogweb.scala
 
-import collection.JavaConversions._
-import org.analogweb._
-import org.analogweb.core._
-import java.util
+import org.analogweb.core.DefaultInvocationMetadata
 
 class ScalaInvocationMetadata(clazz: Class[_], name: String, argumentTypes: Array[Class[_]], val route: Route)
     extends DefaultInvocationMetadata(clazz, name, argumentTypes, route.path) {
