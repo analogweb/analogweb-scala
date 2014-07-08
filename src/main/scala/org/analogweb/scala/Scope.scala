45a1b4a99f21af7f1e77aba6dfdc90da44865e0d
snowgooseyk <snowgoose.yk@gmail.com>
2014-07-08 23:12:58 +0900

    Add arias.

--- src/main/scala/org/analogweb/scala/Scope.scala
+++ src/main/scala/org/analogweb/scala/Scope.scala
@@ -7,6 +7,10 @@ case class Scope[T <: RequestValueResolver](val resolverType: Class[T], val r: R
 
   def valueOf(name: String): Option[String] = valueAs[String](name)
 
+  def valueAs[T](implicit ctag: ClassTag[T]): Option[T] = {
+    valueAs("")(ctag)
+  }
+
   def valueAs[T](name: String)(implicit ctag: ClassTag[T]): Option[T] = {
     Some(r.resolvers.findRequestValueResolver(resolverType)).map { resolver =>
       Option(resolver.resolveValue(r.context, r.metadata, name, ctag.runtimeClass, Array()).asInstanceOf[T])
