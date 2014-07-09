36582e357b1f54267fa8f516e68bab06b8732daf
snowgooseyk <snowgoose.yk@gmail.com>
2014-07-09 23:05:15 +0900

    Rename

--- src/main/scala/org/analogweb/scala/Scope.scala
+++ src/main/scala/org/analogweb/scala/Scope.scala
@@ -5,13 +5,13 @@ import org.analogweb.RequestValueResolver
 
 case class Scope[T <: RequestValueResolver](val resolverType: Class[T], val r: Request) {
 
-  def valueOf(name: String): Option[String] = valueAs[String](name)
+  def of(name: String): Option[String] = as[String](name)
 
-  def valueAs[T](implicit ctag: ClassTag[T]): Option[T] = {
-    valueAs("")(ctag)
+  def as[T](implicit ctag: ClassTag[T]): Option[T] = {
+    as("")(ctag)
   }
 
-  def valueAs[T](name: String)(implicit ctag: ClassTag[T]): Option[T] = {
+  def as[T](name: String)(implicit ctag: ClassTag[T]): Option[T] = {
     Some(r.resolvers.findRequestValueResolver(resolverType)).map { resolver =>
       Option(resolver.resolveValue(r.context, r.metadata, name, ctag.runtimeClass, Array()).asInstanceOf[T])
     }.getOrElse(throw new IllegalArgumentException)
