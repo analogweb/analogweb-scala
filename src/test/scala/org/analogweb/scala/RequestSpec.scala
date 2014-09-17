5eb878dedb38019d0abbdd44c78ade074610a33c
snowgooseyk <snowgoose.yk@gmail.com>
2014-09-17 23:08:13 +0900

    Replace to BeforeExample

--- src/test/scala/org/analogweb/scala/RequestSpec.scala
+++ src/test/scala/org/analogweb/scala/RequestSpec.scala
@@ -4,65 +4,69 @@ import org.junit.runner.RunWith
 import org.specs2.mutable._
 import org.specs2.runner.JUnitRunner
 import org.specs2.mock.Mockito
+import org.specs2.specification.BeforeExample
 import org.analogweb._
 
 @RunWith(classOf[JUnitRunner])
-class RequestSpec extends Specification with Mockito {
+class RequestSpec extends Specification with Mockito with BeforeExample {
 
   val factory = new ScalaInvocationFactory
 
-  class Components {
+  var rc = mock[RequestContext]
+  var rvr = mock[RequestValueResolvers]
+  var im = mock[ScalaInvocationMetadata]
+  var tc = mock[TypeMapperContext]
+  var qp = mock[Parameters]
+  var rh = mock[Headers]
+  var request = new Request(rc, rvr, im, tc)
+  
+  def before = {
 
-    lazy val rc = mock[RequestContext]
-    lazy val rvr = mock[RequestValueResolvers]
-    lazy val im = mock[ScalaInvocationMetadata]
-    lazy val tc = mock[TypeMapperContext]
-    lazy val qp = mock[Parameters]
-    lazy val rh = mock[Headers]
-    lazy val request = new Request(rc, rvr, im, tc)
+    rc = mock[RequestContext]
+    rvr = mock[RequestValueResolvers]
+    im = mock[ScalaInvocationMetadata]
+    tc = mock[TypeMapperContext]
+    qp = mock[Parameters]
+    rh = mock[Headers]
+    request = new Request(rc, rvr, im, tc)
 
   }
 
   "Request" should {
     "Returns avairable query parameter" in {
-      val cp = new Components
-      cp.rc.getQueryParameters returns cp.qp
-      cp.qp.getValues("foo") returns java.util.Collections.emptyList()
-      cp.qp.getValues("baa") returns java.util.Arrays.asList("baz")
-      val none = cp.request.query("foo")
+      rc.getQueryParameters returns qp
+      qp.getValues("foo") returns java.util.Collections.emptyList()
+      qp.getValues("baa") returns java.util.Arrays.asList("baz")
+      val none = request.query("foo")
       none === None
-      val actual = cp.request.query("baa")
+      val actual = request.query("baa")
       actual.get === "baz"
     }
     "Returns avairable request header" in {
-      val cp = new Components
-      cp.rc.getRequestHeaders returns cp.rh
-      cp.rh.getValues("x-foo") returns java.util.Collections.emptyList()
-      cp.rh.getValues("x-baa") returns java.util.Arrays.asList("baz")
-      val none = cp.request.header("x-foo")
+      rc.getRequestHeaders returns rh
+      rh.getValues("x-foo") returns java.util.Collections.emptyList()
+      rh.getValues("x-baa") returns java.util.Arrays.asList("baz")
+      val none = request.header("x-foo")
       none === None
-      val actual = cp.request.header("x-baa")
+      val actual = request.header("x-baa")
       actual.get === "baz"
     }
     "Returns content type" in {
-      val cp = new Components
       val mt = mock[MediaType]
       mt.toString returns "application/json"
-      cp.rc.getContentType returns mt
-      val actual = cp.request.content
+      rc.getContentType returns mt
+      val actual = request.content
       actual.get === "application/json"
     }
     "Content type not avairable" in {
-      val cp = new Components
-      cp.rc.getContentType returns null
-      val actual = cp.request.content
+      rc.getContentType returns null
+      val actual = request.content
       actual === None
     }
     "Returns path" in {
-      val cp = new Components
       val rpm = mock[RequestPath]
-      cp.rc.getRequestPath returns rpm
-      val actual = cp.request.requestPath
+      rc.getRequestPath returns rpm
+      val actual = request.requestPath
       actual === rpm
     }
   }
