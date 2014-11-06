b3ffd40b039a5cd20b386cdb6b17cd0d5e05dbca
snowgooseyk <snowgoose.yk@gmail.com>
2014-11-07 00:33:02 +0900

    Fix unstable test cases.

--- src/test/scala/org/analogweb/scala/RequestSpec.scala
+++ src/test/scala/org/analogweb/scala/RequestSpec.scala
@@ -4,36 +4,25 @@ import org.junit.runner.RunWith
 import org.specs2.mutable._
 import org.specs2.runner.JUnitRunner
 import org.specs2.mock.Mockito
-import org.specs2.specification.BeforeExample
 import org.analogweb._
 
 @RunWith(classOf[JUnitRunner])
-class RequestSpec extends Specification with Mockito with BeforeExample {
+class RequestSpec extends Specification with Mockito {
 
   val factory = new ScalaInvocationFactory
 
-  var rc = mock[RequestContext]
-  var rvr = mock[RequestValueResolvers]
-  var im = mock[ScalaInvocationMetadata]
-  var tc = mock[TypeMapperContext]
-  var qp = mock[Parameters]
-  var rh = mock[Headers]
-  var request = new Request(rc, rvr, im, tc)
-
-  def before = {
-
-    rc = mock[RequestContext]
-    rvr = mock[RequestValueResolvers]
-    im = mock[ScalaInvocationMetadata]
-    tc = mock[TypeMapperContext]
-    qp = mock[Parameters]
-    rh = mock[Headers]
-    request = new Request(rc, rvr, im, tc)
-
+  trait mocks extends org.specs2.specification.Scope {
+    var rc = mock[RequestContext]
+    var rvr = mock[RequestValueResolvers]
+    var im = mock[ScalaInvocationMetadata]
+    var tc = mock[TypeMapperContext]
+    var qp = mock[Parameters]
+    var rh = mock[Headers]
+    var request = new Request(rc, rvr, im, tc)
   }
 
   "Request" should {
-    "Returns avairable query parameter" in {
+    "Returns avairable query parameter" in new mocks {
       rc.getQueryParameters returns qp
       qp.getValues("foo") returns java.util.Collections.emptyList()
       qp.getValues("baa") returns java.util.Arrays.asList("baz")
@@ -42,7 +31,7 @@ class RequestSpec extends Specification with Mockito with BeforeExample {
       val actual = request.query("baa")
       actual.get === "baz"
     }
-    "Returns avairable request header" in {
+    "Returns avairable request header" in new mocks {
       rc.getRequestHeaders returns rh
       rh.getValues("x-foo") returns java.util.Collections.emptyList()
       rh.getValues("x-baa") returns java.util.Arrays.asList("baz")
@@ -51,19 +40,19 @@ class RequestSpec extends Specification with Mockito with BeforeExample {
       val actual = request.header("x-baa")
       actual.get === "baz"
     }
-    "Returns content type" in {
+    "Returns content type" in new mocks {
       val mt = mock[MediaType]
       mt.toString returns "application/json"
       rc.getContentType returns mt
       val actual = request.content
       actual.get === "application/json"
     }
-    "Content type not avairable" in {
+    "Content type not avairable" in new mocks {
       rc.getContentType returns null
       val actual = request.content
       actual === None
     }
-    "Returns path" in {
+    "Returns path" in new mocks {
       val rpm = mock[RequestPath]
       rc.getRequestPath returns rpm
       val actual = request.requestPath
