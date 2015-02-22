fd9ba37bbc8914146e1a13dd8567a4f7bd2c5bfe
snowgooseyk <snowgoose.yk@gmail.com>
2015-02-22 22:24:26 +0900

    Add test case. #3

---
+++ src/test/scala/org/analogweb/scala/ScalaModulesConfigSpec.scala
@@ -0,0 +1,27 @@
+package org.analogweb.scala
+
+import org.junit.runner.RunWith
+import org.specs2.mutable._
+import org.specs2.runner.JUnitRunner
+import org.specs2.mock.Mockito
+import org.analogweb._
+
+@RunWith(classOf[JUnitRunner])
+class ScalaModulesConfigSpec extends Specification with Mockito {
+
+  val config = new ScalaModuleConfig
+
+  "ScalaModulesConfig" should {
+    "sertainly configure" in {
+      val mb = mock[ModulesBuilder]
+      mb.addInvocationMetadataFactoriesClass(classOf[ScalaInvocationMetadataFactory]) returns mb
+      mb.setInvocationFactoryClass(classOf[ScalaInvocationFactory]) returns mb
+      mb.addResponseFormatterClass(classOf[ScalaJsonObject], classOf[ScalaJsonFormatter]) returns mb
+      mb.addResponseFormatterClass(classOf[ScalaJsonText], classOf[ScalaJsonFormatter]) returns mb
+      val actual = config.prepare(mb)
+      actual === mb
+    }
+  }
+
+}
+
