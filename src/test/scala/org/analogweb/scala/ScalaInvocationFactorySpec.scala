7f8dcec5ad87f328f5e0077abbacc074e1f5f717
snowgooseyk <snowgoose.yk@gmail.com>
2015-02-23 22:59:54 +0900

    Add test case. #3

--- src/test/scala/org/analogweb/scala/ScalaInvocationFactorySpec.scala
+++ src/test/scala/org/analogweb/scala/ScalaInvocationFactorySpec.scala
@@ -6,12 +6,14 @@ import org.specs2.runner.JUnitRunner
 import org.specs2.mock.Mockito
 import org.analogweb.ContainerAdaptor
 import org.analogweb.InvocationMetadata
+import org.analogweb.Invocation
 import org.analogweb.RequestContext
 import org.analogweb.RequestValueResolvers
 import org.analogweb.RequestValueResolver
 import org.analogweb.TypeMapperContext
 import org.analogweb.ResponseContext
 import org.analogweb.RequestPathMetadata
+import org.analogweb.core.DefaultInvocation
 
 @RunWith(classOf[JUnitRunner])
 class ScalaInvocationFactorySpec extends Specification with Mockito {
@@ -19,11 +21,13 @@ class ScalaInvocationFactorySpec extends Specification with Mockito {
   val factory = new ScalaInvocationFactory
 
   "ScalaInvocationMetadataFactory" should {
-    "Foo contains invocation" in {
+    "Foo contains scala invocation" in {
       val ca = mock[ContainerAdaptor]
       val im = mock[ScalaInvocationMetadata]
       val route = mock[Route]
       im.route returns route
+      val invocationResult = None
+      route.invoke(any[Request]) returns invocationResult 
       val rc = mock[RequestContext]
       val rsc = mock[ResponseContext]
       val tc = mock[TypeMapperContext]
@@ -33,6 +37,22 @@ class ScalaInvocationFactorySpec extends Specification with Mockito {
       actual.getInvocationInstance() === route
       actual.getInvocationArguments() === actual
       actual.getInvocationArguments().asList().isEmpty() must beTrue
+      // nop
+      actual.asInstanceOf[ScalaInvocation].replace(None) 
+      // nop
+      actual.asInstanceOf[ScalaInvocation].putInvocationArgument(1,None) 
+      actual.invoke() === invocationResult 
+    }
+    "Foo not contains java invocation" in {
+      val ca = mock[ContainerAdaptor]
+      val im = mock[InvocationMetadata]
+      im.getInvocationClass.asInstanceOf[Class[AnyRef]] returns classOf[ScalaInvocationFactorySpec].asInstanceOf[Class[AnyRef]]
+      val rc = mock[RequestContext]
+      val rsc = mock[ResponseContext]
+      val tc = mock[TypeMapperContext]
+      val rvr = mock[RequestValueResolvers]
+      val actual = factory.createInvocation(ca, im, rc, rsc, tc, rvr)
+      actual.isInstanceOf[DefaultInvocation] must beTrue
     }
   }
 
