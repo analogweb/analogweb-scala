d7c73d6bca07fd49179dcdd9622220c3130d5c2f
snowgooseyk <snowgoose.yk@gmail.com>
2015-03-24 22:58:41 +0900

    Pull up Route

--- src/main/scala/org/analogweb/scala/Route.scala
+++ src/main/scala/org/analogweb/scala/Route.scala
@@ -2,17 +2,25 @@ package org.analogweb.scala
 
 import org.analogweb.core.RequestPathDefinition
 
-class Route(val method: String, val rawPath: String)(invocation: Request => Any) {
+trait Route {
+
+  val method: String
+
+  val rawPath: String
 
   val path = RequestPathDefinition.define("/", rawPath, Array[String](method))
 
-  def invoke(request: Request): Any = invocation(request)
+  def invoke(request: Request): Any
 
+}
+
+class RequestInvocation(override val method: String, override val rawPath: String)(val invocation: Request => Any) extends Route {
+  override def invoke(request: Request): Any = invocation(request)
 }
 
 object Route {
 
-  def apply(method: String, path: String)(invocation: Request => Any) = new Route(method, path)(invocation)
+  def apply(method: String, path: String)(invocation: Request => Any) = new RequestInvocation(method, path)(invocation)
 
 }
 
