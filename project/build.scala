fb3187799abe27b860667625a7e522110b2126db
snowgooseyk <snowgoose.yk@gmail.com>
2015-04-03 22:57:40 +0900

    Update version

--- project/build.scala
+++ project/build.scala
@@ -7,7 +7,7 @@ import scoverage._
 
 object BuildSettings {
     val buildOrganization = "org.analogweb"
-    val buildVersion      = "0.9.4-SNAPSHOT"
+    val buildVersion      = "0.9.4"
     val buildScalaVersion = "2.10.4"
     val clossBuildScalaVersion = Seq("2.10.4","2.11.4")
 
@@ -46,8 +46,8 @@ object BuildSettings {
     )
 }
 object Dependencies {
-  val core = "org.analogweb" % "analogweb-core" % "0.9.4-SNAPSHOT"
-  val fileupload = "org.analogweb" % "analogweb-commons-fileupload" % "0.9.4-SNAPSHOT"
+  val core = "org.analogweb" % "analogweb-core" % "0.9.4"
+  val fileupload = "org.analogweb" % "analogweb-commons-fileupload" % "0.9.4"
   val jackson = "com.fasterxml.jackson.module" % "jackson-module-scala" % "2.4.3" cross CrossVersion.fullMapped {
       case "2.10.4" => "2.10"
       case "2.11.4" => "2.11"
