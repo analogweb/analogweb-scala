41f5c0766df37080062e14c0569e42e371cee416
snowgooseyk <snowgoose.yk@gmail.com>
2015-08-18 23:50:32 +0900

    Fix test case.

--- src/test/scala/org/analogweb/scala/ScalaModulesConfigSpec.scala
+++ src/test/scala/org/analogweb/scala/ScalaModulesConfigSpec.scala
@@ -5,6 +5,7 @@ import org.specs2.mutable._
 import org.specs2.runner.JUnitRunner
 import org.specs2.mock.Mockito
 import org.analogweb._
+import org.analogweb.core._
 
 @RunWith(classOf[JUnitRunner])
 class ScalaModulesConfigSpec extends Specification with Mockito {
@@ -20,6 +21,9 @@ class ScalaModulesConfigSpec extends Specification with Mockito {
       mb.addResponseFormatterClass(classOf[ScalaJsonText], classOf[ScalaJsonFormatter]) returns mb
       mb.setRenderableResolverClass(classOf[ScalaRenderableResolver]) returns mb
       mb.setResponseHandlerClass(classOf[ScalaResponseHandler]) returns mb
+      mb.ignore(classOf[BindAttributeArgumentPreparator]) returns mb
+      mb.ignore(classOf[ConsumesMediaTypeVerifier]) returns mb
+      mb.ignore(classOf[ScopedMapArgumentPreparator]) returns mb
       val actual = config.prepare(mb)
       actual === mb
     }
